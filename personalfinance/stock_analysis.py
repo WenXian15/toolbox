90e82c4faa6b6b85641fe81be70f845bd31e365f
Wen Xian <38619129+WenXian15@users.noreply.github.com>
2025-01-04 14:25:32 +0800

    Create stock_analysis.py

---
+++ personalfinance/stock_analysis.py
@@ -0,0 +1,50 @@
+# Martin Shrekli
+# income statement 
+# balance sheet
+# cash flow statement
+
+# Link:
+# https://www.sec.gov/search-filings
+
+
+
+
+class stock :
+    def __init__(self, tck):
+        self.tck = tck
+        self.price = price
+        self.outstanding_share = outstanding_share
+        self.marketcap = price*outstanding_share 
+        self.cash = cash # Balance sheet
+        self.debt = debt # Balance sheet  
+        self.enterprise_value = marketcap + cash - debt  
+        
+	def valuation() :
+        if (enterprise_value > marketcap):
+        	print("Company is worth more")   
+                
+	def income_statement():
+        
+	def balance_sheet():
+		# asset + liabilities + shareholder equity 
+        asset = 
+        if (marketcap>asset):
+             print("Expecting more revenue in the future")
+            
+        # acc_payable = opposite of account receivable.  Money to pay back for the product/service received
+        # accrued_exps = employee's long term expenses
+        # deffered_revenue = unearned revenue / cash upfront. ie. gift cards. Software companies are more commno to have deffered revenue 
+        # 
+        liabilities =	"acc_payable" + 
+         			  	"accrued_exps" +
+   					  	"deffered_revenue" +     
+        			  	"commercial_paper" +
+                        "debt"
+        
+        
+        
+        
+        _
+        
+    def cash_flow():
+        
